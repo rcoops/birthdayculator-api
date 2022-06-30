69ff242b685d8123134b34831e0b36447ca391b1
Rick Cooper <rick.cooper@upside.energy>
2022-06-30 21:14:44 +0100

    chore: hooks and comitlint

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const { build } = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
-const { Generator } = require('npm-dts');
 
 const isDev = process.argv.some((a) => a === '--dev');
 
-const shared = {
+
+build({
   entryPoints: ['src/index.ts'],
   platform: 'node',
   sourcemap: isDev,
@@ -13,24 +13,7 @@ const shared = {
   bundle: true,
   watch: isDev,
   plugins: [nodeExternalsPlugin()],
-};
-
-build({
-  ...shared,
   outfile: 'dist/index.js',
 }).catch(() => {
   process.exit(1);
 });
-
-build({
-  ...shared,
-  outfile: 'dist/index.esm.js',
-  format: 'esm',
-}).catch(() => {
-  process.exit(1);
-});
-
-new Generator({
-  entry: 'index.ts',
-  output: 'dist/index.d.ts',
-}).generate();
